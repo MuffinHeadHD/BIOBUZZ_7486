57b7c6e44348482a86af18589c7a332442aeccc4
MuffinHeadHD <muffinheadhd@gmail.com>
2026-07-09 19:29:27 -0400

    Update AutonomousRobot.kt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousRobot.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousRobot.kt
@@ -7,27 +7,6 @@ import com.qualcomm.robotcore.util.ElapsedTime
 import org.firstinspires.ftc.teamcode.parts.IntakeMode
 
 class AutonomousRobot(opMode: OpMode): Robot(opMode) {
-    inner class SpindexerRotate(val rotation: Int): Action {
-        private var ran = false
-        private val timer = ElapsedTime()
-
-        override fun run(p: TelemetryPacket): Boolean {
-            if (!ran) {
-                p.put("Spindexer (first)", spindexer.isFinished())
-                spindexer.rotate(rotation)
-                timer.reset()
-                ran = true
-                return true
-            } else {
-                p.put("Spindexer (ran)", spindexer.isFinished())
-                p.put("Spindexer (servo)", spindexer.servo.servo.power)
-                p.put("Spindexer (target)", spindexer.servo.targetPosition)
-                p.put("Spindexer (current)", spindexer.servo.position)
-                p.put("Spindexer (time)", timer.seconds())
-                return !spindexer.isFinished() && timer.seconds() > 0.35
-            }
-        }
-    }
 
     inner class SetIntakeIn(): Action {
         override fun run(p: TelemetryPacket): Boolean {
